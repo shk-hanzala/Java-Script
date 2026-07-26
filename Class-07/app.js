7ed09bf024d5a6c9aff7915d6ecb687161514658
hanzala4t2 <hanzala4t2@gmail.com>
2026-07-27 00:14:47 +0500

    [200~JS-Revision-Assignment-01

--- Class-07/app.js
+++ Class-07/app.js
@@ -16,4 +16,49 @@ console.log(num1 % num2);
 
 //QUESTION 03
 let count = 10;
+//Post increament
+console.log(count++);
+console.log(count);    //Output = 10
+
+//Post decreament
+console.log(count--);
+console.log(count);    //Output = 11
+
+//Pre increament
+console.log(++count);
+console.log(count);    //Output = 11
+
+//Pre decreament
+console.log(--count);
+console.log(count);    //Output = 10
+
+//QUESTION 04
+let x = 5;
+console.log(x++);      //Output = 5
+console.log(++x);      //Output = 7
+console.log(x);        //Output = 7
+
+//QUESTION 05
+
+console.log((10 + 5) * 2);  //Output = 30
+console.log(20 - 10 / 2);   //Output = 15
+console.log((20 - 10) / 2); //Output = 5
+
+//QUESTION 06
+let firstName = "Muhammad";
+let lastName = "Hanzala";
+console.log("Hello", firstName +" "+ lastName);
+
+//QUESTION 07
+let username = prompt("enter your name");
+let usercity = prompt("enter your city");
+console.log("Hello", username);
+console.log("Welcome to", usercity);
+
+//QUESTION 09
+let fpl = prompt("favorite programming language");
+console.log("Your favourite programming language is", fpl);
+
+
+
 
