a372d815129808c51e79eee4ade2a4cd29379a49
hanzala4t2 <hanzala4t2@gmail.com>
2026-07-26 23:41:36 +0500

    practice

---
+++ Class-07/app.js
@@ -0,0 +1,19 @@
+//QUESTION 01
+let firstname = "Muhammad";
+let lastname = "Hanzala";
+let age = 21;
+let city = "Karachi";
+console.log(firstname, lastname, age, city);
+
+//QUESTION 02
+let num1 = 50;
+let num2 = 10;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+//QUESTION 03
+let count = 10;
+
