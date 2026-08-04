de1a23c3dc26347a2dfc7354a21e950e3924b963
hanzala4t2 <hanzala4t2@gmail.com>
2026-08-05 00:06:36 +0500

    Array Revision

--- Class-10/app.js
+++ Class-10/app.js
@@ -1,1 +1,11 @@
-let names = [hanzala]
+//ARRAY
+let names = ["Hanzala", "Sheraz", "Bilal"]
+console.log(names);
+
+// Update ARRAY
+names[1] = "Hassan";
+names[2] = "Zain ul Abiden";
+console.log(names);
+
+// Call specific persons
+console.log(names[0]);
